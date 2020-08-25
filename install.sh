8baf862a89e8929715988cd85a7f63df4abd85f2
by-student-2017 <studentsctest@gmail.com>
2020-08-25 16:13:14 +0900

    Debian 10.0

--- install.sh
+++ install.sh
@@ -3,18 +3,34 @@
 echo "Install libraries"
 sudo apt update
 sudo apt install -y gfortran
-sudo apt install -y gcc g++ build-essential
+sudo apt install -y gcc
+sudo apt install -y g++
+sudo apt install -y build-essential
 sudo apt install -y libopenmpi-dev
-sudo apt install -y libscalapack-openmpi-dev libscalapack-openmpi2.0
-sudo apt install -y libblas-dev liblapack-dev libopenblas-dev libarpack2-dev
+sudo apt install -y libscalapack-openmpi-dev
+sudo apt install -y libscalapack-openmpi2.0
+sudo apt install -y libblas-dev 
+sudo apt install -y liblapack-dev
+sudo apt install -y libopenblas-dev
+sudo apt install -y libarpack2-dev
 sudo apt install -y libfftw3-dev
 sudo apt install -y libxc-dev
-sudo apt install -y git wget make cmake
-sudo apt install -y python-dev python-distutils  python-setuptools
-sudo apt install -y python-numpy python-scipy python-f2py
-sudo apt install -y python-mpmath python-matplotlib
+sudo apt install -y git
+sudo apt install -y wget
+sudo apt install -y make
+sudo apt install -y cmake
+sudo apt install -y python-dev
+sudo apt install -y python-distutils
+sudo apt install -y python-setuptools
+sudo apt install -y python-numpy
+sudo apt install -y python-scipy
+sudo apt install -y python-f2py
+sudo apt install -y python-mpmath
+sudo apt install -y python-matplotlib
 sudo apt install -y python-sympy
-sudo apt install -y grace jmol gnuplot
+#sudo apt install -y grace
+#sudo apt install -y jmol
+#sudo apt install -y gnuplot
 
 echo " "
 echo "DFTBaby install"
